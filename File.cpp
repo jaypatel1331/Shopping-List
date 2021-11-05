06ba2ba8d0558c3434c3f5282a8ea43af4961eea
jaypatel1331 <82784261+jaypatel1331@users.noreply.github.com>
2021-11-05 09:24:21 -0700

    Update File.cpp

--- File.cpp
+++ File.cpp
@@ -1,13 +1,3 @@
-/*********************************************************
- * Name: Jay Ashishbhai Patel
- * Student ID: 154925192
- * Seneca email: japatel31@myseneca.ca
- * Section: OOP 244 NGG
- * 
- * I have done all the coding by myself and only copied the code
- *  that my professor provided to complete my workshops and assignments.
- **********************************************************/
-
 #include <iostream>
 #include <cstdio>
 #include "File.h"
