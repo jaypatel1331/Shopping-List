c0610c7a67b48b083b83ae4a8e38dc1bff81100c
jaypatel1331 <82784261+jaypatel1331@users.noreply.github.com>
2021-11-05 09:22:41 -0700

    Update ShoppingRec.h

--- ShoppingRec.h
+++ ShoppingRec.h
@@ -1,14 +1,3 @@
-/*********************************************************
- * Name: Jay Ashishbhai Patel
- * Student ID: 154925192
- * Seneca email: japatel31@myseneca.ca
- * Section: OOP 244 NGG
- * 
- * I have done all the coding by myself and only copied the code
- *  that my professor provided to complete my workshops and assignments.
- **********************************************************/
-
-
 #ifndef sdds_ShoppingRec_H // replace with relevant names
 #define sdds_ShoppingRec_H
 
